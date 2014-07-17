537b73701ee110e3ef8591177b9ee0b7d908ba34
zzeleznick <zzeleznick@gmail.com>
2014-07-17 13:09:25 +0200

    Gamification
    hot or not

--- imager/js/app.js
+++ imager/js/app.js
@@ -79,7 +79,7 @@ var data = [{
 		sunny: 0,
 		foggy: 0
 	}];
-
+		count = 0;
 		var links = [];
 		var ref = new Firebase("https://hotnot.firebaseio.com/neighborhoods");
 
@@ -90,6 +90,24 @@ var data = [{
 	 	var counts = []; //loaded on first page init
 	 	var allRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/");	
 	 	
+	 	var auth = new FirebaseSimpleLogin(allRef, function(error, user) {
+		  if (error) {
+		    // an error occurred while attempting login
+		    console.log(error);
+		  } else if (user) {
+		    // user authenticated with Firebase
+		    console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
+		  } else {
+		    // user is logged out
+		  }
+		});
+
+		auth.login('facebook', {
+		  rememberMe: false,
+		  scope: 'email,user_likes'
+		});
+
+
 	 	allRef.once('value', function(snap) {  ///Note this will loop
   		       var myc = snap.val();
   		       console.log('my content is: ' + myc);
@@ -98,6 +116,7 @@ var data = [{
 
 	
 	function updateSun(id) { 
+	count = id;
 	var newSun = ++counts[0][id].sunny;
 		var nRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/" + id);
 			var onComplete = function(error) {
@@ -105,13 +124,15 @@ var data = [{
 				  	alert('Synchronization failed.');
 				  } 
 				  else { 
-				  	alert('Synchronization succeeded.');
+				  	//alert('Synchronization succeeded.');
 				  }
 				};
 		 nRef.update({sunny: newSun }, onComplete);	
+		 count = 0;
 	};
 
 	function updateFog(id) {	
+		count = id;
 		var newFog = ++counts[0][id].foggy;
 		var nRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/" + id);
 			var onComplete = function(error) {
@@ -119,47 +140,75 @@ var data = [{
 				  	alert('Synchronization failed.');
 				  } 
 				  else { 
-				  	alert('Synchronization succeeded.');
+				  	//alert('Synchronization succeeded.');
 				  }
 				};
 		 nRef.update({foggy: newFog }, onComplete);	
+		 count = 0;
 	};
 
 
  	$(window).on('load', function(){
  		console.log('loaded');
 
-
  		for (keys in data) {
- 			var myString = "#" + data[keys].id;
- 			var sunRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/" + keys + "/sunny");
- 			var fogRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/" + keys + "/foggy");
- 			//console.log('key at:' + keys);
- 			//console.log('sunRef at:' + sunRef);
- 			links.push(sunRef);
+ 		
+ 			var myString = "#" + data[count].id;
+		    var parRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/");
+ 			var fogRef = new Firebase("https://hotnot.firebaseio.com/neighborhoods/-JS0sS2Ie-ebCoI8FuLG/data/" + count + "/foggy");
+ 			console.log('key at:' + keys);
+ 			console.log('count at:' + count);
+ 			links.push(parRef);
+ 		
+ 		
 
- 		var count = 0;
-		var fcount = 0;
-
-			sunRef.on('value', function(snap) {  ///Note this will looop
-  		       var mySunVal = snap.val();
-  		    //   console.log("my sun val = " + mySunVal);
-  		       var myString = "#" + data[count].id;
+			parRef.on('value', function upHTML(snap) {  ///Note this will looop
+			   var myNeighID =  parRef.child(count);  //url of child	aka the .../index of the neighborhood
+  		      	console.log("id is:  " + myNeighID);
+  		      	console.log("count is: " + count);
+  		      	var myString = "#" + data[count].id;
+  		        console.log("count is:  " + count);
+  		         //console.log("my snap element is: " + snap.child(count).val());
+  	  		     var myNeigh = snap.child(count);
+  	  		     var myName = myNeigh.child('value');
+  	  		    // var myString = myNeigh.child('id').value;
+  	  		    var mySun = myNeigh.child('sunny').val();
+  	  		     var myFog = myNeigh.child('foggy').val();
+  	  		    
+  	  		      console.log("with name " + myName.val());
+  	  		       //console.log("with val " + myName.val());
   		       count++;
-  		    //    console.log("my span string = " + myString);
-  		       $(myString +'-s').text('sunny: ' + mySunVal);
- 			//	console.log('sunny appended');
-			});
+  		       console.log("my sun val = " + mySun);
+  		       console.log("my fog val = " + myFog);
+  		    
+  		       $(myString +'-s').text('sunny: ' + mySun);
+  		       $(myString +'-f').text('foggy: ' + myFog);
+ 			     //console.log('sunny appended');
+			
+			 		 		//Check values and update weather image
+			console.log("fog val = " + myNeigh.child('foggy').val());
+			if (mySun >= myNeigh.child('foggy').val() ) {
+				$(myString).addClass('sunny');
+			} else {
+				$(myString).removeClass('sunny');
+				$(myString).addClass('foggy');
+			}
+ 		    //end img check 
 
+			});
+/*
  		fogRef.on('value', function(snap) {  ///Note this will looop
-  		       var myFogVal = snap.val();
-  		      // console.log("my fog val = " + myFogVal);
-  		       var myString = "#" + data[fcount].id;
-  		       fcount++;
+  		       console.log("count is: " + count);
+  		    	var myFogVal = snap.val();
+  		      console.log("my fog val = " + myFogVal);
+  		       var myString = "#" + data[count].id;
   		     //   console.log("my span string = " + myString);
   		       $(myString +'-f').text('foggy: ' + myFogVal);
  			//	console.log('foggy appended');
 			});
+
+ 		//end fogRef call
+ */
  		}
  	});
 
---
+++ weather-ang/js/app.js
@@ -0,0 +1,87 @@
+var myapp = angular.module("myapp", ["firebase"]);
+
+function WeatherController($scope, $firebase) {
+	$scope.hoods = [{
+		value: 'The Richmond',
+		id: 'the-richmond'
+	}, {
+		value: 'The Presidio',
+		id: 'the-presidio'
+	}, {
+		value: 'North Beach',
+		id: 'north-beach'
+	}, {
+		value: 'Golden Gate Park',
+		id: 'ggp'
+	}, {
+		value: 'NOPA',
+		id: 'nopa'
+	}, {
+		value: 'Tenderloin',
+		id: 'tenderloin'
+	}, {
+		value: 'The Sunset',
+		id: 'the-sunset'
+	}, {
+		value: 'The Haight',
+		id: 'the-haight'
+	}, {
+		value: 'SOMA',
+		id: 'soma'
+	}, {
+		value: 'The Mission',
+		id: 'the-mission'
+	}, {
+		value: 'Financial District',
+		id: 'fidi'
+	}, {
+		value: 'Nob Hill',
+		id: 'nob-hill',
+	}, {
+		value: 'Russian Hill',
+		id: 'russian-hill'
+	}, {
+		value: 'Chinatown',
+		id: 'chinatown'
+	}, {
+		value: 'The Marina',
+		id: 'marina'
+	}, {
+		value: 'Hayes Valley',
+		id: 'hayes-valley'
+	}];
+}
+
+function NeighborhoodController($scope, $firebase, Firebase) {
+   $scope.init = function(neighborhood) {
+      // bind Firebase data to scope variable 'data'
+      var FBURL = "https://hotnot.firebaseio.com/ang/";
+      $scope.data = $firebase(new Firebase(FBURL + neighborhood));
+
+      // monitor data for updates and check weather setting
+      $scope.data.$on('loaded', checkWeather);
+      $scope.data.$on('change', checkWeather);
+
+		$scope.updateCount = function(forecast) {
+			//Increment counter in Firebase using a transaction
+         var forecastRef = new Firebase(FBURL + neighborhood);
+			forecastRef.transaction(function(current_val) {
+            // initialize the data if this neighborhood hasn't been saved before
+            if( !current_val ) { current_val = {sunny: 0, foggy: 0, forecast: 0}; }
+
+            current_val.forecast++;
+            current_val[forecast]++;
+            return current_val;
+			});
+      };
+   };
+
+	//Check values to display sun or fog image
+   function checkWeather() {
+      if ($scope.data.sunny >= $scope.data.foggy) {
+			$scope.weather = "sunny";
+		} else {
+			$scope.weather = "foggy";
+		}
+	}
+}
