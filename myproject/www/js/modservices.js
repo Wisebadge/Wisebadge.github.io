8dcffe1740f9e38c964a244376675ec6800a0008
Roger Kerse <rogerkerse@gmail.com>
2014-07-23 02:29:54 +0200

    A lot of refactoring

--- myproject/www/js/modservices.js
+++ myproject/www/js/modservices.js
@@ -14,57 +14,68 @@ angular.module('starter.modservices', [])
     { id: 3, name: 'Ash Ketchum' }
   ]; */
 
-myMatches = [{
+myMatches = [
+    {
         'name': 'Tiffine Wang',
         'email': 't@wang.com',
         'phone': '214-748-3647',
         'title': 'iOS Engineer at Apple',
         'location': 'San Francisco, CA',
         'id': 0,
-        'date': (new Date).toJSON()
-    }, {
-        'name': 'John Smith',
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Tiffine.jpg'
+    },
+    {
+        'name': 'Alar Kolk',
+        'email': 'alarkolk@inacadamy.eu',
+        'phone': '123-456-7890',
+        'title': 'CEO at EIA',
+        'location': 'Tallinn, Estonia',
+        'id': 1,
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Alar.jpg'
+    },
+    {
+        'name': 'Chris',
+        'email': 'chris@smith.com',
+        'phone': '123-456-7890',
+        'title': 'Consultant',
+        'location': 'San Francisco, CA',
+        'id': 2,
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Chris.jpg'
+    },
+    {
+        'name': 'Fred Krieger',
         'email': 'j@smith.com',
         'phone': '123-456-7890',
-        'title': 'UI/UX at Airbnb',
-        'location': 'Berkeley, CA',
-        'id': 1,
-        'date': (new Date).toJSON()
-    }, {
+        'title': 'UI/UX at Scoro',
+        'location': 'Tallinn, Estonia',
+        'id': 3,
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Fred.jpg'
+    },
+    {
         'name': 'Helen Poldsman',
         'email': 'j@smith.com',
         'phone': '123-456-7890',
         'title': 'Product Design at IDEO',
         'location': 'Berkeley, CA',
-        'id': 2,
-        'date': (new Date).toJSON()
-    }, {
-        'name': 'Elon Musk',
-        'email': 'e@musk.com',
-        'phone': '123-456-7890',
-        'title': 'Founder at SpaceX',
-        'location': 'Cupertino, CA',
-        'id': 3,
-        'date': (new Date).toJSON()
-    },
-    {
-        'name': 'Q Smith',
-        'email': 'j@smith.com',
-        'phone': '123-456-7890',
-        'title': 'Product Design at IDEO',
-        'location': 'Berkeley, CA',
         'id': 4,
-        'date': (new Date).toJSON()
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Helen.jpg'
     },
     {
-        'name': 'Z Smith',
+        'name': 'Ravi',
         'email': 'j@smith.com',
         'phone': '123-456-7890',
         'title': 'Product Design at IDEO',
         'location': 'Berkeley, CA',
         'id': 5,
-        'date': (new Date).toJSON()
-    }, ];
+        'date': (new Date).toJSON(),
+        'image': 'http://wisebadge.github.io/myproject/www/img/Ravi.jpg'
+    }
+  ];
 
   return {
     all: function() {
